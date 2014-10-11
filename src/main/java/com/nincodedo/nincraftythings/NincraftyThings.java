80d47993d12e78c52c9a22c571026f13fe56e87f
Nincodedo <nincodedo@gmail.com>
2014-10-11 16:18:37 -0500

    Added GUI config

--- src/main/java/com/nincodedo/nincraftythings/NincraftyThings.java
+++ src/main/java/com/nincodedo/nincraftythings/NincraftyThings.java
@@ -14,7 +14,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION,guiFactory=Reference.GUI_FACTORY_CLASS)
 public class NincraftyThings {
 
 	@Mod.Instance(Reference.MOD_ID)
--- src/main/java/com/nincodedo/nincraftythings/reference/Reference.java
+++ src/main/java/com/nincodedo/nincraftythings/reference/Reference.java
@@ -6,4 +6,5 @@ public class Reference {
 	public static final String VERSION = "1.7.10-1.0";
 	public static final String CLIENT_PROXY = "com.nincodedo.nincraftythings.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "com.nincodedo.nincraftythings.proxy.ServerProxy";
+	public static final String GUI_FACTORY_CLASS = "com.nincodedo.nincraftythings.client.gui.GuiFactory";
 }
