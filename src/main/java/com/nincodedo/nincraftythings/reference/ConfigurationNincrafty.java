2b5d16e83096b532df35ec013f2d03aeb7215f49
Nincodedo <nincodedo@gmail.com>
2014-10-20 01:33:53 -0500

    More Refactoring

---
+++ src/main/java/com/nincodedo/nincraftythings/reference/ConfigurationNincrafty.java
@@ -0,0 +1,8 @@
+package com.nincodedo.nincraftythings.reference;
+
+public class ConfigurationNincrafty {
+
+	public static final String CATEGORY_NINCODIUM_ARMOR = "armor.nincodium";
+	
+	public static final String CATEGORY_NINCODIUM_ARMOR_REDUCTION = "armor.nincodium.damage reduction";
+}
--- src/main/java/com/nincodedo/nincraftythings/reference/NincraftyConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.nincodedo.nincraftythings.reference;
-
-public class NincraftyConfiguration {
-
-	public static final String CATEGORY_NINCODIUM_ARMOR = "armor.nincodium";
-
-}
