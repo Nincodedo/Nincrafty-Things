3fa2a5a2f40b0b0add1a4daaf31b3d412b885dcd
Nincodedo <nincodedo@gmail.com>
2014-11-11 01:23:33 -0600

    Removed Nincodium ingot temp crafting recipe
    Because dat was waay too easy maan... also never meant to be that.

--- src/main/java/com/nincodedo/nincraftythings/init/Recipes.java
+++ src/main/java/com/nincodedo/nincraftythings/init/Recipes.java
@@ -12,8 +12,7 @@ public class Recipes {
 	public static void init() {
 
 		if (Settings.Items.enableNincodiumIngot) {
-			GameRegistry.addSmelting(Items.diamond, new ItemStack(
-					ModItems.nincodiumIngot), 1);
+			
 		}
 
 		if (Settings.Items.enableNincodiumTools) {
