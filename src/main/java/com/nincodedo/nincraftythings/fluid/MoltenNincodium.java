326ec8f2657ce9180a009ff0da42b10aa6acbeed
Nincodedo <nincodedo@gmail.com>
2014-12-14 02:26:59 -0600

    Formatting plz

--- src/main/java/com/nincodedo/nincraftythings/fluid/MoltenNincodium.java
+++ src/main/java/com/nincodedo/nincraftythings/fluid/MoltenNincodium.java
@@ -17,37 +17,41 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class MoltenNincodium extends BlockFluidClassic {
 
-	 @SideOnly(Side.CLIENT)
-     protected IIcon stillIcon;
-     @SideOnly(Side.CLIENT)
-     protected IIcon flowingIcon;
-	
+	@SideOnly(Side.CLIENT)
+	protected IIcon stillIcon;
+	@SideOnly(Side.CLIENT)
+	protected IIcon flowingIcon;
+
 	public MoltenNincodium(Fluid fluid, Material material) {
 		super(fluid, material);
-        setCreativeTab(CreativeTabNincrafty.NINCRAFTY_TAB);
+		setCreativeTab(CreativeTabNincrafty.NINCRAFTY_TAB);
 	}
 
 	@Override
-    public IIcon getIcon(int side, int meta) {
-            return (side == 0 || side == 1)? stillIcon : flowingIcon;
-    }
-    
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void registerBlockIcons(IIconRegister register) {
-            stillIcon = register.registerIcon(Reference.MOD_ID+":moltenNincodiumStill");
-            flowingIcon = register.registerIcon(Reference.MOD_ID+":moltenNincodiumFlowing");
-    }
-    
-    @Override
-    public boolean canDisplace(IBlockAccess world, int x, int y, int z) {
-            if (world.getBlock(x,  y,  z).getMaterial().isLiquid()) return false;
-            return super.canDisplace(world, x, y, z);
-    }
-    
-    @Override
-    public boolean displaceIfPossible(World world, int x, int y, int z) {
-            if (world.getBlock(x,  y,  z).getMaterial().isLiquid()) return false;
-            return super.displaceIfPossible(world, x, y, z);
-    }
+	public IIcon getIcon(int side, int meta) {
+		return (side == 0 || side == 1) ? stillIcon : flowingIcon;
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerBlockIcons(IIconRegister register) {
+		stillIcon = register.registerIcon(Reference.MOD_ID
+				+ ":moltenNincodiumStill");
+		flowingIcon = register.registerIcon(Reference.MOD_ID
+				+ ":moltenNincodiumFlowing");
+	}
+
+	@Override
+	public boolean canDisplace(IBlockAccess world, int x, int y, int z) {
+		if (world.getBlock(x, y, z).getMaterial().isLiquid())
+			return false;
+		return super.canDisplace(world, x, y, z);
+	}
+
+	@Override
+	public boolean displaceIfPossible(World world, int x, int y, int z) {
+		if (world.getBlock(x, y, z).getMaterial().isLiquid())
+			return false;
+		return super.displaceIfPossible(world, x, y, z);
+	}
 }
