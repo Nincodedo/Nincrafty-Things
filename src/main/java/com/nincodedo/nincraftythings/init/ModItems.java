f1b675c0b08fb4dcf2710fddb74e7de07892b891
Nincodedo <nincodedo@gmail.com>
2014-12-14 13:21:22 -0600

    Actually can be placed now

--- src/main/java/com/nincodedo/nincraftythings/init/ModItems.java
+++ src/main/java/com/nincodedo/nincraftythings/init/ModItems.java
@@ -1,10 +1,13 @@
 package com.nincodedo.nincraftythings.init;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
 
 import com.nincodedo.nincraftythings.armor.ItemArmorNincodium;
 import com.nincodedo.nincraftythings.armor.ItemArmorNincrafty;
+import com.nincodedo.nincraftythings.fluid.MoltenNincodium;
 import com.nincodedo.nincraftythings.item.ItemNincodiumIngot;
 import com.nincodedo.nincraftythings.item.ItemNincrafty;
 import com.nincodedo.nincraftythings.reference.Reference;
@@ -22,6 +25,7 @@ public class ModItems {
 	public static ItemArmorNincrafty nincodiumLeggings;
 	public static ItemArmorNincrafty nincodiumBoots;
 	public static Fluid moltenNincodium;
+	public static Block moltenNincodiumBlock;
 
 	public static void init() {
 		nincodiumIngot = new ItemNincodiumIngot();
@@ -33,6 +37,9 @@ public class ModItems {
 		moltenNincodium = new Fluid("moltenNincodium");
 		
 		FluidRegistry.registerFluid(moltenNincodium);
+		moltenNincodiumBlock = new MoltenNincodium(moltenNincodium, Material.water).setBlockName("moltenNincodium");
+		GameRegistry.registerBlock(moltenNincodiumBlock, "moltenNincodiumBlock");
+		
 		GameRegistry.registerItem(nincodiumIngot, "nincodiumIngot");
 		GameRegistry.registerItem(nincodiumPickaxe, "nincodiumPickaxe");
 		GameRegistry.registerItem(nincodiumHelmet, "nincodiumHelmet");
