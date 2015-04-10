2a56e814f53a17cb59539f41a43f168a2b33eb98
Nincodedo <nincodedo@gmail.com>
2015-04-10 14:55:05 +0000

    Some ore things
    Will have to play with this in game to see how it works. Also need to check harvest levels

--- src/main/java/com/nincodedo/nincraftythings/block/BlockNincodiumOre.java
+++ src/main/java/com/nincodedo/nincraftythings/block/BlockNincodiumOre.java
@@ -5,6 +5,8 @@ import com.nincodedo.nincraftythings.reference.Names;
 
 public class BlockNincodiumOre extends BlockNincrafty {
 	public BlockNincodiumOre() {
+		setHardness(3.0F);
+		setResistance(5.0F);
 		setBlockName(Names.Blocks.NINCODIUM_ORE);
 		setCreativeTab(CreativeTabNincrafty.NINCRAFTY_TAB);
 	}
